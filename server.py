98e2ff7c7aa0cd9d4f51d3cb1a3672681fe70fff
jam <jammoben@gmail.com>
2023-01-09 21:26:54 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+tokenizer = AutoTokenizer.from_pretrained(
+    "jamm55/autotrain-pidgintranslationmix-2798982563"
+    )
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    "jamm55/autotrain-pidgintranslationmix-2798982563"
+    )
+app = Flask(__name__)
+
+@app.route('/predict', methods=["GET","POST"])
+def predict():
+    # Get the input data from the request body
+    #data = request.json
+
+    input_text = "When are we going to the zoo?"
+
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+
+    # Generate translations
+    output = model.generate(input_ids)
+
+    # Decode the generated translations
+    predicted_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    print(predicted_text)
+
+    # Return the prediction as a JSON response
+    return jsonify({'prediction': predicted_text})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
