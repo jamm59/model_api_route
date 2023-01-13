fc02812d89c4c9373de37b41ba0184310e6af7e0
jam <jammoben@gmail.com>
2023-01-13 14:33:24 +0000

    update 2 for model implementation

--- api/index.py
+++ api/index.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained(
-    "jamm55/autotrain-improved-pidgin-model-2837583189"
-    )
-model = AutoModelForSeq2SeqLM.from_pretrained(
-    "jamm55/autotrain-improved-pidgin-model-2837583189"
-    )
+#from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 app = Flask(__name__)
+
+# # initialize the tokenizer and model
+# tokenizer = AutoTokenizer.from_pretrained(
+#     "jamm55/autotrain-improved-pidgin-model-2837583189"
+# )
+# model = AutoModelForSeq2SeqLM.from_pretrained(
+#     "jamm55/autotrain-improved-pidgin-model-2837583189"
+# )
 
 @app.route('/')
 def home():
@@ -18,15 +19,19 @@ def home():
 def about():
     return 'About'
 
-@app.route('/predict', methods=["GET"])
-def predict():
-    # Get the input data from the request body
-    #data = request.json
-    input_text = "When are we going to the zoo?"
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    # Generate translations
-    output = model.generate(input_ids)
-    # Decode the generated translations
-    predicted_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    # Return the prediction as a JSON response
-    return jsonify({'prediction': predicted_text})
+# @app.route('/predict', methods=["POST"])
+# def predict():
+#     # Get the input data from the request body
+#     data = request.json
+#     input_text = data['input_text']
+#     # tokenize the input
+#     input_ids = tokenizer.encode(input_text, return_tensors='pt')
+#     # Generate translations
+#     output = model.generate(input_ids)
+#     # Decode the generated translations
+#     predicted_text = tokenizer.decode(output[0], skip_special_tokens=True)
+#     # Return the prediction as a JSON response
+#     return jsonify({'prediction': predicted_text})
+
+# if __name__ == '__main__':
+#     app.run()
