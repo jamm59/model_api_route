d99dca89bcb411bb9bf617267768d117fb394844
jam <jammoben@gmail.com>
2023-01-14 10:01:14 +0000

    update 8 I think it works now

--- api/index.py
+++ api/index.py
@@ -1,43 +1,33 @@
-import os
-from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-os.environ["TRANSFORMERS_CACHE"] = '/.cache/huggingface/transformers'
+import requests
+from flask import Flask, request, jsonify , url_for
 
 app = Flask(__name__)
 
-# initialize the tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained(
-    "jamm55/autotrain-improved-pidgin-model-2837583189"
-)
-model = AutoModelForSeq2SeqLM.from_pretrained(
-    "jamm55/autotrain-improved-pidgin-model-2837583189"
-)
+
+API_URL = "https://api-inference.huggingface.co/models/jamm55/autotrain-improved-pidgin-model-2837583188"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    # payloads = {"inputs": "how are you doing today?"}
+    # output = requests.post("http://localhost:5000/predict",json=payloads)
+    return "Hello, World"
 
-@app.route('/about')
-def about():
-    return 'About'
 
-@app.route('/favicon.ico')
-def favicon():
-    return '', 404
 
-# @app.route('/predict', methods=["POST"])
-# def predict():
-#     # Get the input data from the request body
-#     data = request.json
-#     input_text = data['input_text']
-#     # tokenize the input
-#     input_ids = tokenizer.encode(input_text, return_tensors='pt')
-#     # Generate translations
-#     output = model.generate(input_ids)
-#     # Decode the generated translations
-#     predicted_text = tokenizer.decode(output[0], skip_special_tokens=True)
-#     # Return the prediction as a JSON response
-#     return jsonify({'prediction': predicted_text})
+@app.route('/predict', methods=["POST"])
+def predict():
+    # Get the input data from the request body
+    data = request.json
+    print(data)
+    output = query(data)
+    # Return the prediction as a JSON response
+    return jsonify({'prediction': output})
 
 if __name__ == '__main__':
     app.run(debug=True)
